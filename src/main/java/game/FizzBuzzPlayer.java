47a673477192dfef193471ce2606c4c1f380eb15
SON NGUYEN <myantj3m@gmail.com>
2018-08-20 11:47:26 +0700

    add Fizz Buzz Game

---
+++ src/main/java/game/FizzBuzzPlayer.java
@@ -0,0 +1,26 @@
+package game;
+
+public class FizzBuzzPlayer {
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
+
+    private static String normalAnswer(int number) {
+        return String.valueOf(number);
+    }
+
+    public static String answer(int number) {
+        if (isFizz(number)) {
+            return FIZZ;
+        } else if (isBuzz(number)) {
+            return BUZZ;
+        }
+        return normalAnswer(number);
+    }
+
+    private static boolean isFizz(int number) {
+        return number == 3;
+    }
+    private static boolean isBuzz(int number) {
+        return number == 5;
+    }
+}
